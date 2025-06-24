c50dfe8d4e66a19bf1c77cff109d541fe7b0f084
Rafoolin <niloo.jv@gmail.com>
2025-06-24 17:59:50 +0200

    feat: add more configs for SelCNN

--- fcn_tracker/models/selcnn.py
+++ fcn_tracker/models/selcnn.py
@@ -12,15 +12,28 @@ class SelCNN(nn.Module):
     Predicts a 2D heatmap from VGG feature maps using a simple regression model.
     """
 
-    def __init__(self, in_channels: int = 512, dropout: float = 0.5):
-        """
-        Args:
-            in_channels (int): Number of feature maps from VGG layer (e.g., 512).
-            dropout (float): Dropout rate.
-        """
+    def __init__(
+        self,
+        in_channels: int = 512,
+        out_channels: int = 1,
+        dropout_rate: float = 0.3,
+        kernel_size: int = 3,
+        padding: int = 1,
+        weight_std: float = 1e-7,
+        bias_value: float = 0.0,
+    ):
+
         super(SelCNN, self).__init__()
-        self.dropout = nn.Dropout2d(p=dropout)
-        self.conv1x1 = nn.Conv2d(in_channels, 1, kernel_size=1, bias=False)
+        self.dropout = nn.Dropout2d(p=dropout_rate)
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+        )
+
+        nn.init.normal_(self.conv.weight, mean=0.0, std=weight_std)
+        nn.init.constant_(self.conv.bias, bias_value)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -33,4 +46,4 @@ class SelCNN(nn.Module):
             Tensor: Output saliency map of shape (B, 1, H, W)
         """
         x = self.dropout(x)
-        return self.conv1x1(x)
+        return self.conv(x)
